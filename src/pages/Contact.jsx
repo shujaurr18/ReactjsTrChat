08adba8e6bf2b7fb6f9a1430d624454b0eb11dfb
shuja <shujaurr18@gmail.com>
2024-12-03 21:33:45 +0500

    Added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,14 +5,14 @@ const Contact = () => {
         window.scrollTo(0, 0);
       }, []);
   return (
-    <div className="min-h-screen flex flex-col ">
+    <div className=" flex flex-col ">
       {/* Header */}
       <div className="bg-[#8B3E2F] py-2">
         <h1 className="text-white text-center text-3xl font-normal">Contact</h1>
       </div>
       
       {/* Main Content */}
-      <div className="flex-1 container mx-auto px-4 md:px-8 lg:px-16 md:py-16 py-6 items-center flex">
+      <div className="flex-1 container mx-auto px-4 md:px-8 lg:px-16 md:py-16 py-6  ">
         <div className="grid grid-cols-1 md:grid-cols-2 md:gap-16 gap-6">
           {/* Left Column - Text */}
           <div className="text-gray-600 leading-relaxed">
